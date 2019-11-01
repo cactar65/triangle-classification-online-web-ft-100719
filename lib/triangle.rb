c957fd036f403435947071c278920894266f3369
Nicholas Walton <cactar65@gmail.com>
2019-11-01 00:46:52 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,7 +2,14 @@ class Triangle
   
   attr_reader :equilateral, :isosceles, :scalene
   
-  
+  def initialize(side1, side2, side3)
+    @s1 = side1
+    @s2 = side2
+    @s3 = side3
+    
+    
+    
+  end
   
   
   class TriangleError < StandardError
