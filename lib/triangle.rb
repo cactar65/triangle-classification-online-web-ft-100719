dd9bd418149af6d69781757b57c18712db988ae5
Nicholas Walton <cactar65@gmail.com>
2019-11-01 01:30:10 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,7 +9,7 @@ class Triangle
     
     def kind
       
-      if (@s1 <= 0 || @s2 <= 0 || @s3 <= 0) || (@s1 + @s2)
+      if (@s1 <= 0 || @s2 <= 0 || @s3 <= 0) || (@s1 + @s2 <= @s3 || @s2+)
       
     end
     
