e7c77264d79d25b5432da8c814a111e4b30e4025
Nicholas Walton <cactar65@gmail.com>
2019-11-01 00:37:30 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,5 +1,12 @@
 class Triangle
   
-  attr_reader :equilateral, :isosceles, :c
+  attr_reader :equilateral, :isosceles, :scalene
+  
+  
+  
+  
+  class TriangleError < StandardError
+  
+  end
   
 end
